89d072785d0f119e879ad5e14a6b4888710dda88
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-03-24 20:44:10 +0430

    feat: write enums classes to levels and roles

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,5 @@
 from .config import settings
+from .enums import Level, Role
 from .versioning import VersionFastAPI
 from .middleware import catch_exceptions
 from .security import oauth2_schema, jwt_auth, AuthJWT
