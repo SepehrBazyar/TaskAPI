52edd4b7d132f53e0ee4f689af33038348991b0e
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-03-24 19:21:55 +0430

    feat: add core package and env setting vars

---
+++ core/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Settings Class to Read Environment Variable Parameters"""
+
+    # Security
+    BASE_URL: str
+    ALGORITHM: str
+    SECRET_KEY: str
+    ALLOWED_HOSTS: str
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DEBUG: bool
+
+    # Databases
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
