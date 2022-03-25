fe56e67b938ee764c0b876a6bb38dc490e46ad98
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-03-25 23:57:21 +0430

    refactor: change inheritance task model schemas

--- schemas/task.py
+++ schemas/task.py
@@ -7,48 +7,44 @@ from .member import MemberBriefSchema
 from .project import ProjectBriefSchema
 
 
-class TaskInDBSchema(BaseModel):
-    """Schema to Create New Task with Creator ID as Owner Role"""
-
-    name: str = Field(min_length=3, max_length=64)
-    description: Optional[str] = Field(default=None)
-    start_date: Optional[date] = Field(default=None)
-    end_date: Optional[date] = Field(default=None)
-    is_halted: bool = Field(default=True)
-    member_id: UUID
-    project_id: UUID
-
-
-class TaskOutDBSchema(BaseModel):
-    """Schema to Retriever Task with Creator ID as Owner Role"""
-
-    name: str = Field(min_length=3, max_length=64)
-    description: Optional[str] = Field(default=None)
-    start_date: Optional[date] = Field(default=None)
-    end_date: Optional[date] = Field(default=None)
-    is_halted: bool = Field(default=True)
-    member: MemberBriefSchema
-    project: ProjectBriefSchema
-
-
-class TaskUpdateSchema(BaseModel):
-    """Schema for Update Task Fields All is Optional Items Remove Unsets"""
-
-    name: Optional[str] = Field(default=None, min_length=3, max_length=64)
-    description: Optional[str] = Field(default=None)
-    start_date: Optional[date] = Field(default=None)
-    end_date: Optional[date] = Field(default=None)
-    is_halted: Optional[bool] = Field(default=None)
-    member_id: Optional[UUID] = Field(default=None)
-    project_id: Optional[UUID] = Field(default=None)
-
-
 class TaskBriefSchema(UUIDSchema):
     """Schema of the Brief Detials Task to Showing in Tasks List"""
 
     name: str = Field(min_length=3, max_length=64)
     member: MemberBriefSchema
     project: ProjectBriefSchema
+
+
+class OptionalFieldSchema(BaseModel):
+    """Schema to Shared Fields of Project Model is Optional Can be None"""
+
+    description: Optional[str] = Field(default=None)
+    start_date: Optional[date] = Field(default=None)
+    end_date: Optional[date] = Field(default=None)
+
+
+class TaskInDBSchema(OptionalFieldSchema):
+    """Schema to Create New Task with Creator ID as Owner Role"""
+
+    name: str = Field(min_length=3, max_length=64)
+    is_halted: bool = Field(default=True)
+    member_id: UUID
+    project_id: UUID
+
+
+class TaskOutDBSchema(TaskBriefSchema, OptionalFieldSchema):
+    """Schema to Retriever Task with Creator ID as Owner Role"""
+
+    is_halted: bool = Field(default=True)
+
+
+class TaskUpdateSchema(OptionalFieldSchema):
+    """Schema for Update Task Fields All is Optional Items Remove Unsets"""
+
+    name: Optional[str] = Field(default=None, min_length=3, max_length=64)
+    is_halted: Optional[bool] = Field(default=None)
+    member_id: Optional[UUID] = Field(default=None)
+    project_id: Optional[UUID] = Field(default=None)
 
 
 class TaskListSchema(Pagination):
