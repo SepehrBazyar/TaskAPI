49b353f9beaae8317d5327410caf4be3f90c8d06
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-03-28 01:53:38 +0430

    fix: fixing errors on cbv generics without schema

--- api/base.py
+++ api/base.py
@@ -1,5 +1,7 @@
 import ormar
 from fastapi import Depends, Request, HTTPException, status
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
 from abc import ABC
 from core import ItemsPerPage, DBPagination
 from models import User
@@ -10,20 +12,19 @@ class BaseAPIView(ABC):
     """Abstract Base View Class to Shared Dependencies etc Authentication"""
 
     # Shared Dependencies As a Class Atrribiutes Access from Self Parameter
-    current_user: User = Depends(get_current_user, use_cache=True)
+    # current_user: User = Depends(get_current_user, use_cache=True)
 
 
 class ListCreateAPIView(BaseAPIView):
     """Class Based View for List & Create Operations for Entity Models"""
 
-    path: str = "/"
-    model: ormar.Model
+    model: ormar.Model = User
 
     async def list(
         self,
         request: Request,
         pagination: ItemsPerPage = Depends(),
-    ) -> model.Schema.List:
+    ):
         """Returned the List of Entity Model with Brief Details in Pagination Mode"""
 
         count: int = await self.model.objects.count()
@@ -40,3 +41,38 @@ class ListCreateAPIView(BaseAPIView):
             "previous": previous,
             "results": await self.model.objects.all(),
         }
+
+
+class Generics:
+    """PASS"""
+
+    def __init__(self, router: InferringRouter, model: ormar.Model):
+        self.router, self.model = router, model
+
+    def list_create(self, path: str = "/"):
+        """PASS"""
+
+        @cbv(self.router)
+        class Inner:
+            @self.router.get(path, status_code=status.HTTP_200_OK)
+            async def list(
+                self,
+                request: Request,
+                pagination: ItemsPerPage = Depends(),
+            ):
+                """Returned the List of Entity Model with Brief Details in Pagination Mode"""
+
+                count: int = await self.model.objects.count()
+                paginate = DBPagination(url=request.url, total=count, paginations=pagination)
+                if not await paginate.is_valid_page():
+                    raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND, detail="Page Not Found."
+                    )
+
+                next, previous = await paginate.next_and_previous()
+                return {
+                    "count": count,
+                    "next": next,
+                    "previous": previous,
+                    "results": await self.model.objects.all(),
+                }
--- api/v1_0_0/views/user.py
+++ api/v1_0_0/views/user.py
@@ -1,4 +1,15 @@
 from fastapi_utils.inferring_router import InferringRouter
+from models import User
+from api.base import ListCreateAPIView, Generics
 
 
 router = InferringRouter()
+
+class UserListCreateAPIView(ListCreateAPIView):
+    """PASS"""
+
+    model = User
+
+
+generic = Generics(router, model=User)
+generic.list_create()
