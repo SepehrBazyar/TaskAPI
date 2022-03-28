348eff8d0d155659261abcbc3032485108ca54e2
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-03-28 18:11:08 +0430

    feat: writing login route response jwt tokens

--- api/v1_0_0/views/user.py
+++ api/v1_0_0/views/user.py
@@ -1,10 +1,39 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_utils.inferring_router import InferringRouter
-from models import UserSerializer
+from core import jwt_auth
+from models import User, UserSerializer
+from schemas import (
+    AccessTokenSchema,
+    RefreshTokenSchema,
+)
 from api.base import GenericAPIView
 
 
 router = InferringRouter()
-
-
 generic = GenericAPIView(router, serializer=UserSerializer)
+
+
+@router.post(
+    "/login/",
+    status_code=status.HTTP_200_OK
+)
+async def login(
+    form: OAuth2PasswordRequestForm = Depends(),
+) -> RefreshTokenSchema:
+    """Authorize View Get Username & Password to Login User Returned JWT Token"""
+
+    user = await User.objects.get_or_none(mboile=form.username, is_active=True)
+    if user is not None and await user.sign_in(password=form.password):
+        return {
+            "access_token": await jwt_auth.create_access_token(user_id=user.id),
+            "refresh_token": await jwt_auth.create_refresh_token(user_id=user.id),
+        }
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Phone Number or Password Incorrect.",
+    )
+
+
 generic.list_create()
