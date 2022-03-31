e09b2870716ba9dbd31f9b86e262538f29a86aa9
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-04-01 02:02:49 +0430

    refactor: change orm project from encode to ormar

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .team import Team
 from .member import TeamUser
-# from .project import Project
+from .project import Project
 # from .task import Task
 # from .work import Work
