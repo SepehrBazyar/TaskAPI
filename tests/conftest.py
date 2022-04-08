190cf32443bcc81cf68aedde3cb67f645be4c970
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-04-08 22:27:03 +0430

    fix: use async engine for fixture create all

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,27 @@
 from pytest import fixture
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from asyncio import get_event_loop
 from core import settings
 from db import metadata
 
 
-engine = create_engine(
-    url=settings.SQLITE_TEST_URL,
-    connect_args={
-        "check_same_thread": False,
-    },
-)
+engine = create_async_engine(settings.SQLITE_TEST_URL, echo=True)
 
 
 @fixture(autouse=True, scope="session")
-def create_test_database():
+async def create_test_database():
     """Basic SetUp and TearDown for Clearing DataBase"""
 
-    metadata.create_all(bind=engine)
-    yield
-    metadata.drop_all(bind=engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+        yield
+        await conn.run_sync(metadata.drop_all)
+
+
+@fixture(autouse=True, scope="session")
+def event_loop():
+    """Override the Event Loop Tests for Set Async Fixture Tests"""
+
+    loop = get_event_loop()
+    yield loop
+    loop.close()
