75493adedcd851c1dc3ff2cabb828dfc5ea3c014
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-04-09 13:51:06 +0430

    test: resume test case user model classes

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -1,9 +1,17 @@
+from uuid import UUID
 from models import User
 from schemas import UserInDBSchema
 
 
-class TestUser:
+class TestUserModel:
+    """Test Cases Class to Methods of User Class Model Entity ORM"""
+
+    __MOBILE, __PASSWORD = "9123456789", "qwerty1234"
+
     async def test_create_user(self):
-        form = UserInDBSchema(mobile="9123456789", password="1234567890")
+        form = UserInDBSchema(mobile=self.__MOBILE, password=self.__PASSWORD)
         user = await User.sign_up(form=form)
-        assert user.id is not None
+
+        assert user is not None
+        assert isinstance(user.id, UUID)
+        assert user.mobile == self.__MOBILE
