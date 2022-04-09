92b3a03c2a8f092734c256cf042cbe7fe4711c88
SepehrBazyar <bazyar.sepehr@gmail.com>
2022-04-09 21:50:09 +0430

    fix: write function fixture to get jwt token

--- tests/api/v1_0_0/test_user.py
+++ tests/api/v1_0_0/test_user.py
@@ -1,7 +1,15 @@
 from httpx import AsyncClient
+from typing import Dict
+from ...conftest import VERSIONS
+
+
+URL_STR = VERSIONS.get("1.0.0") + "user/"
 
 
 class TestUserRoutes:
-    async def test_user(self, client: AsyncClient):
-        response = await client.get("/v1.0.0/user/")
-        assert response.status_code == 401
+    async def test_get_user(
+        self,
+        client: AsyncClient,
+        admin_token_headers: Dict[str, str],
+    ):
+        response = await client.get(URL_STR, headers=admin_token_headers)
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,35 @@ from httpx import AsyncClient
 from pytest_asyncio import fixture
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from asyncio import get_event_loop
-from typing import Generator
-from core import settings
+from typing import Dict, Generator
+from core import settings, Level
 from db import metadata
+from models import User
+from schemas import UserInDBSchema
 from main import app
 
 
 engine: AsyncEngine = create_async_engine(settings.SQLITE_TEST_URL, echo=True)
+
+FIRST_ADMIN = {
+    "username": "9928917653",
+    "password": "sepehrbazyar",
+}
+
+VERSIONS = {
+    "1.0.0": "/v1.0.0/",
+}
+
+
+async def insert_first_admin():
+    """Inserting First Admin User to Access Protected Routes View"""
+
+    form = UserInDBSchema(
+        level=Level.ADMIN,
+        mobile=FIRST_ADMIN.get("username"),
+        password=FIRST_ADMIN.get("password"),
+    )
+    await User.sign_up(form=form)
 
 
 @fixture(autouse=True, scope="session")
@@ -26,6 +48,7 @@ async def clean_tables_database():
     """Clearing Database Tables to Delete All Rows for Each Test Case"""
 
     try:
+        await insert_first_admin()
         yield
     finally:
         async with engine.begin() as conn:
@@ -39,6 +62,18 @@ async def client() -> Generator[AsyncClient, None, None]:
 
     async with AsyncClient(app=app, base_url=settings.BASE_URL) as client:
         yield client
+
+
+@fixture(scope="session")
+async def admin_token_headers(client: AsyncClient) -> Dict[str, str]:
+    """PASS"""
+
+    await insert_first_admin()
+    response = await client.post(f"/user/login/", data=FIRST_ADMIN)
+    tokens: Dict[str, str] = response.json()
+    return {
+        "Authorization": f"Bearer {tokens.get('access_token')}",
+    }
 
 
 @fixture(autouse=True, scope="session")
